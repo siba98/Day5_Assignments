39783f9fcdccdb12c9a85dd5aa3e258f91971354
siba98 <gyansiba.patro@gmail.com>
2021-09-15 21:02:36 +0530

    Siba: added swapping between two numbers

---
+++ Swapping2Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignments
+{
+    class Swapping2Numbers
+    {
+        public static void Swapping()
+        {
+            int num1, num2;
+            Console.WriteLine("Enter the first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before Swapping the numbers are: " + "Number1 = " + num1 + "," + " Number2 = " + num2);
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine("After swapping the numbers are: " + "Number1 = " + num1 + "," + " Number2 = " + num2);
+        }
+    }
+}
