03ee3faa3a2c1a514e5f65ad2a37b746822d57e1
siba98 <gyansiba.patro@gmail.com>
2021-09-15 21:12:02 +0530

    Siba: added quotient and reminder programm

---
+++ Quotient_Remainder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignments
+{
+    class Quotient_Remainder
+    {
+        public static void solution()
+        {
+            int dividend, divisor;
+            Console.Write("Enter a number as dividend: ");
+            dividend = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number as divisor: ");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend = " + dividend + " Divisor = " + divisor);
+            Console.WriteLine("The quotient is: " + quotient);
+            Console.WriteLine("The remainder is: " + remainder);
+        }
+    }
+}
