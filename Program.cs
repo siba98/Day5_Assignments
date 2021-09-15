2a35625a48f338676ed40fd1b74f297167ef5384
siba98 <gyansiba.patro@gmail.com>
2021-09-15 21:18:38 +0530

    Siba: added main method compilation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day5_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quotient_Remainder.solution();
+            //Swapping2Numbers.Swapping();
+            //EvenOdd.NumCheck();
+            //Vowel_Consonant.CheckChar();
+            //LargestNumber.FindLargest();
+            //Leap_Year.DetermineLeapYear();
+            //FlipCoin_HeadTail.HeadTail();
+            //Harmonic_Number.HarmonicNum();
+            PowerOf2.PrintPower();
+        }
+    }
+}
