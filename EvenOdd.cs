b6d9d6f9ed96ead283dfcd5d4474965e2e2b18b7
siba98 <gyansiba.patro@gmail.com>
2021-09-15 20:54:52 +0530

    Siba: added even odd programm

---
+++ EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Assignments
+{
+    class EvenOdd
+    {
+        public static void NumCheck()
+        {
+            int num;
+            Console.WriteLine("Enter a number to check");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+        }
+    }
+}
